f5c7015af60c0514279b170b921386a43f510d86
Yuliia Parashchenko <yuliia.parashchenko@gmail.com>
2023-04-25 00:06:17 +0300

    react todoapp

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
-import { App } from './App';
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
+import './styles/index.css';
+import './styles/todo-list.css';
+import './styles/filters.css';
+
+import { HashRouter, Route, Routes } from 'react-router-dom';
+import { App } from './App';
+import { TodoApp } from './components/TodoApp';
+
+const root = createRoot(document.getElementById("root") as HTMLElement);
+
+root.render(
+  <HashRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<TodoApp />} />
+        <Route path="completed" element={<TodoApp />} />
+        <Route path="active" element={<TodoApp />} />
+      </Route>
+    </Routes>
+  </HashRouter>,
+  // document.getElementById('root'),
 );
